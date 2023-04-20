3bc357e1bde87ccd4994dbd8b9f531f852465769
Landon Hammond <lthammond@bsu.edu>
2023-04-20 16:08:17 -0400

    Add Route and Stop Repositories

---
+++ Core/Repositories/RouteRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Models;
+
+namespace Core.Repositories
+{
+    public class RouteRepository
+    {
+        private readonly ShuttleDbContext _dbContext;
+
+        public RouteRepository(ShuttleDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Route CreateRoute(Route route)
+        {
+            _dbContext.Add(route);
+            _dbContext.SaveChanges();
+            return route;
+        }
+
+        public Route GetRoute(int routeId)
+        {
+            return _dbContext.Set<Route>().Find(routeId);
+        }
+
+        public IEnumerable<Route> GetAllRoutes()
+        {
+            return _dbContext.Set<Route>().ToList();
+        }
+
+        public Route UpdateRoute(Route route)
+        {
+            _dbContext.Entry(route).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return route;
+        }
+
+        public void DeleteRoute(int routeId)
+        {
+            Route route = _dbContext.Set<Route>().Find(routeId);
+            if (route != null)
+            {
+                _dbContext.Set<Route>().Remove(route);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Core/Repositories/StopRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Models;
+
+namespace Core.Repositories
+{
+    public class StopRepository
+    {
+        private readonly ShuttleDbContext _dbContext;
+
+        public StopRepository(ShuttleDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Stop CreateStop(Stop stop)
+        {
+            _dbContext.Add(stop);
+            _dbContext.SaveChanges();
+            return stop;
+        }
+
+        public Stop GetStop(int stopId)
+        {
+            return _dbContext.Set<Stop>().Find(stopId);
+        }
+
+        public IEnumerable<Stop> GetAllStops()
+        {
+            return _dbContext.Set<Stop>().ToList();
+        }
+
+        public Stop UpdateStop(Stop stop)
+        {
+            _dbContext.Entry(stop).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return stop;
+        }
+
+        public void DeleteStop(int stopId)
+        {
+            Stop stop = _dbContext.Set<Stop>().Find(stopId);
+            if (stop != null)
+            {
+                _dbContext.Set<Stop>().Remove(stop);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
