8da51566c73347f6196810eed7ffc26ab294eefe
Jacob Dye <jacobadye1@gmail.com>
2023-04-25 14:35:10 -0400

    Create basic login with routing

---
+++ View/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace View.Controllers;
+
+public class AccountController : Controller
+{
+    public ActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "password")
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        ViewBag.ErrorMessage = "Invalid username or password.";
+            return View();
+    }
+}
